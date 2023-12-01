c7d11a78cf161a2d21174c0a51269e663b8ced27
herrJones <1090348+herrJones@users.noreply.github.com>
2023-12-01 21:19:30 +0100

    Update bionic-aarch64.sh
    activate jetsonnano options in Autobuild.sh

--- Autobuild/bionic-aarch64.sh
+++ Autobuild/bionic-aarch64.sh
@@ -1,2 +1,10 @@
 source Autobuild/aarch64.sh
+
+if [ -f /etc/nv_tegra_release ]; then
+  echo "identified NVidia Jetson"
+  AUTOBUILD_CONFIGURE_EXTRA="${AUTOBUILD_CONFIGURE_EXTRA} --enable-jetsonnano --disable-ffmpeg_static"
+
+  echo " ** ${AUTOBUILD_CONFIGURE_EXTRA}"
+fi
 source Autobuild/bionic.sh
+
